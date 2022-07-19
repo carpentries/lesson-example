9c7f411a6e678e9b39ca613d080b5b88d94755f6
Pip Grylls <pipgryllsastro@gmail.com>
2022-07-19 15:59:00 +0100

    working on autoschedules

--- bin/get_schedules.py
+++ bin/get_schedules.py
@@ -137,10 +137,16 @@ def create_detailed_lesson_schedules(lesson_name, lesson_type, start_time, lesso
         if "99-" in file:
             with open(f"{containing_directory}/{new_file_name}", 'r') as fp:
                 data = fp.readlines()
-            if data[2] == "slug: lesson-survey\n":
-                data[2] = f"slug: {lesson_name}-survey\n"
-                with open(f"{containing_directory}/{new_file_name}", 'w') as fp:
-                    fp.writelines(data)
+            try:
+                with data.index("slug: lesson-survey\n") as ix:
+                    if lesson_name == '':
+                        data[ix] = f"slug: {lesson_title}-survey\n"
+                    else:
+                        data[ix] = f"slug: {lesson_name}-survey\n"
+                    with open(f"{containing_directory}/{new_file_name}", 'w') as fp:
+                        fp.writelines(data)
+            except ValueError as e:
+                print(f"No survey markdown found, caught: {e}\n continuing")
 
     if website_kind != 'lesson':
         schedule_markdown = textwrap.dedent(f"""---
